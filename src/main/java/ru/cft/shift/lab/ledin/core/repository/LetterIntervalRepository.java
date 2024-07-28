bdc14a06c8b3b2ba465af32a860f97fcb276bd9d
Ледин Даниил Александрович <daniil.ledkin@mail.ru>
2024-07-29 02:41:18 +0700

    Add repository

---
+++ src/main/java/ru/cft/shift/lab/ledin/core/repository/LetterIntervalRepository.java
@@ -0,0 +1,15 @@
+package ru.cft.shift.lab.ledin.core.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.cft.shift.lab.ledin.core.entity.impl.LetterInterval;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface LetterIntervalRepository extends JpaRepository<LetterInterval, UUID> {
+    @Query(value = "SELECT * FROM letter_interval ORDER BY interval_start, interval_end ASC FETCH FIRST 1 ROW ONLY;", nativeQuery = true)
+    Optional<LetterInterval> findMinInterval();
+}
