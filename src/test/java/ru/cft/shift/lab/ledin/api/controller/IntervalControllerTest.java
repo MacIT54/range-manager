76cfe1c362896b2041f3362835d086dd69502ee0
Ледин Даниил Александрович <daniil.ledkin@mail.ru>
2024-07-29 21:31:01 +0700

    Add tests for controller

--- src/test/java/ru/cft/shift/lab/ledin/api/controller/IntervalControllerTest.java
+++ src/test/java/ru/cft/shift/lab/ledin/api/controller/IntervalControllerTest.java
@@ -1,19 +1,24 @@
 package ru.cft.shift.lab.ledin.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.Test;
+import org.springdoc.core.utils.PropertyResolverUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.cft.shift.lab.ledin.api.dto.IntervalDto;
 import ru.cft.shift.lab.ledin.core.service.IntervalService;
 
-@AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @WebMvcTest(controllers = IntervalController.class)
 public class IntervalControllerTest {
 
@@ -23,13 +28,65 @@ public class IntervalControllerTest {
     @MockBean
     private IntervalService intervalService;
 
+    @MockBean
+    private PropertyResolverUtils propertyResolverUtils;
+
     @Autowired
     private ObjectMapper objectMapper;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @Test
+    public void testMergeIntervalsDigit() throws Exception {
+        List<IntervalDto<?>> intervals = List.of(new IntervalDto<>(1, 5), new IntervalDto<>(6, 10));
+        doNothing().when(intervalService).mergeIntervals(eq("digits"), any());
+
+        mockMvc.perform(post("/api/v1/intervals/merge")
+                        .param("kind", "digits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(intervals)))
+                .andExpect(status().isOk());
+
+        verify(intervalService, times(1)).mergeIntervals(eq("digits"), any());
     }
 
+    @Test
+    public void testGetMinIntervalDigit() throws Exception {
+        when(intervalService.getMinInterval("digits")).thenReturn((IntervalDto) new IntervalDto<>(1, 5));
 
+        mockMvc.perform(get("/api/v1/intervals/min")
+                        .param("kind", "digits")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").value(1))
+                .andExpect(jsonPath("$[1]").value(5));
+
+        verify(intervalService, times(1)).getMinInterval("digits");
+    }
+
+    @Test
+    public void testMergeIntervalsLetter() throws Exception {
+        List<IntervalDto<?>> intervals = List.of(new IntervalDto<>("a", "f"), new IntervalDto<>("d", "j"));
+        doNothing().when(intervalService).mergeIntervals(eq("letters"), any());
+
+        mockMvc.perform(post("/api/v1/intervals/merge")
+                        .param("kind", "letters")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(intervals)))
+                .andExpect(status().isOk());
+
+        verify(intervalService, times(1)).mergeIntervals(eq("letters"), any());
+    }
+
+    @Test
+    public void testGetMinIntervalLetter() throws Exception {
+        when(intervalService.getMinInterval("letters")).thenReturn((IntervalDto) new IntervalDto<>("a", "f"));
+
+        mockMvc.perform(get("/api/v1/intervals/min")
+                        .param("kind", "letters")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").value("a"))
+                .andExpect(jsonPath("$[1]").value("f"));
+
+        verify(intervalService, times(1)).getMinInterval("letters");
+    }
 }
